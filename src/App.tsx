ed5299bec0c06b484315fd6cd312d220c1891bbd
Skylar Bolton <skylar.bolton@gmail.com>
2023-02-02 21:31:59 -0500

    Swap icons add scss support

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './vim-what.svg'
+import './App.scss'
+import { Key } from './Key'
+import { Grid } from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Grid container spacing={2}>
+          <Grid item>
+            <Key value='q' description='A' keyType={'command'} />
+          </Grid>
+          <Grid item>
+            <Key value='w' description='A' keyType={'motion'} />
+          </Grid>
+          <Grid item>
+            <Key value='e' description='A' keyType={'motion'} />
+          </Grid>
+          <Grid item>
+            <Key value='r' description='A' keyType={'command'} />
+          </Grid>
+          <Grid item>
+            <Key value='t' description='A' keyType={'motion'} />
+          </Grid>
+          <Grid item>
+            <Key value='y' description='A' keyType={'operator'} />
+          </Grid>
+        </Grid>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Vim... What?
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Key.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card } from '@mui/material'
+
+export type KeyType = 'motion' | 'operator' | 'command' | 'extra'
+
+type KeyProps = {
+  value: string
+  description: string
+  keyType: KeyType
+  disabled?: boolean
+  hasDot?: boolean
+  hasBorder?: boolean
+  notes?: string[]
+}
+export const Key = ({
+  value,
+  description,
+  keyType = 'motion',
+  disabled,
+  hasDot,
+  hasBorder,
+  notes,
+}: KeyProps) => {
+  return <Card className={`key ${keyType}`}>{value}</Card>
+}
